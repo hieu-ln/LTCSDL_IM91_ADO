a93cba6894ddde49e02deed7c75128c23e1de18b
Hao Lee <occbuu@gmail.com>
2021-10-25 10:46:28 +0700

    Commit lan dau cho lop IM91 git add .!

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("XIn Chao lop IM91 - LTCSDL !!!");
+            string cnStr = "Server = localhost; Database = Northwind; User id = sa; password = Password123;";
+            SqlConnection cnn = new SqlConnection(cnStr);
+            try
+            {
+                
+                cnn.Open();
+                Console.WriteLine("Ket noi thanh cong toi DB !!!");
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cnn;
+                // Cau 1 - du lieu dang bảng
+                string sqlStr = "select CategoryID, CategoryName from Categories";
+                cmd.CommandText = sqlStr;
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                string str = "";
+                while(reader.Read())
+                {
+                    str +="\t"+ reader["CategoryID"] + " - " + reader["CategoryName"] + "\n";
+                }
+                reader.Close();
+                Console.Write(str);
+
+                // Cau 2 - Du lieu dang đơn - scalar
+                sqlStr = "select count(*) as Total from Categories;";
+                cmd.CommandText = sqlStr;
+                int total = (int)cmd.ExecuteScalar();
+                Console.WriteLine(" --> Total of Categories: " + total);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Ket noi khong thanh cong !!!");
+                Console.WriteLine("Loi:" + e.Message);
+            }
+            if (cnn.State == System.Data.ConnectionState.Open)
+            {
+                cnn.Close();
+                Console.WriteLine("Ket noi da ngat !!!");
+            }
+        }
+    }
+}
